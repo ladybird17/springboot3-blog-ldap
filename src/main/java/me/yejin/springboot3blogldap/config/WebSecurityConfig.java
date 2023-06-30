def2ecd0fc2f04cd4a60bbfd8666051ff6dd3063
yjseo <yjseo@iochord.com>
2023-06-30 18:28:36 +0900

    feat: 역할에 따른 삭제 권한

--- src/main/java/me/yejin/springboot3blogldap/config/WebSecurityConfig.java
+++ src/main/java/me/yejin/springboot3blogldap/config/WebSecurityConfig.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,15 +35,15 @@ public class WebSecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     //특정 HTTP 요청에 대한 웹 기반 보안 구성
     return http.authorizeHttpRequests(authorize -> authorize
-          .requestMatchers("/login", "/signup", "/user").permitAll()
+          .requestMatchers("/login").permitAll()
+          .requestMatchers(HttpMethod.DELETE, "/api/articles/*").hasRole("MANAGERS")
           .anyRequest().authenticated()
         )//인증, 인가 설정
         .formLogin(login -> login.loginPage("/login").permitAll().defaultSuccessUrl("/articles")
         )//폼 기반 로그인 설정
         .logout(logout -> logout.logoutSuccessUrl("/login").invalidateHttpSession(true)
         )//로그아웃 설정
-        .csrf(csrf -> csrf.disable()
-        )//csrf 비활성화
+        .csrf(AbstractHttpConfigurer::disable)//csrf 비활성화
         .build();
   }
 
